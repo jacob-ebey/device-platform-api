647b9b1445d54d879f191f374d6c56d2eddc6f22
Jacob Ebey <jacob.ebey@live.com>
2017-11-18 12:34:58 -0800

    - Updated auth controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,9 +1,4 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import httpStatus from 'http-status';
-import APIError from '../helpers/APIError';
-import config from '../../config/config';
-import User from '../models/user.model';
+import users from '../repos/users';
 
 /**
  * Returns jwt token if valid username and password is provided
@@ -13,48 +8,12 @@ import User from '../models/user.model';
  * @returns {*}
  */
 function login(req, res, next) {
-  if (req.body) {
-    User.findOne({ username: req.body.username }, (error, user) => {
-      if (error || !user) {
-        const err = new APIError(error || 'Username or password is not correct', httpStatus.BAD_REQUEST, true);
-        next(err);
-        return;
-      }
-
-      bcrypt.compare(req.body.password, user.password, (result, success) => {
-        if (success) {
-          const token = jwt.sign({
-            username: user.username,
-            _id: user._id
-          }, config.jwtSecret);
-          res.json({
-            token,
-            username: user.username
-          });
-        } else {
-          const err = new APIError('Username or password is not correct', httpStatus.UNAUTHORIZED, true);
-          next(err);
-        }
-      });
-    });
-  } else {
-    const err = new APIError('Authentication error', httpStatus.UNAUTHORIZED, true);
-    next(err);
-  }
+  users
+    .login(req.body)
+    .then((result) => {
+      res.json(result);
+    })
+    .catch(e => next(e));
 }
 
-/**
- * This is a protected route. Will return random number only if jwt token is provided in header.
- * @param req
- * @param res
- * @returns {*}
- */
-function getRandomNumber(req, res) {
-  // req.user is assigned by jwt middleware if valid token is provided
-  return res.json({
-    user: req.user,
-    num: Math.random() * 100
-  });
-}
-
-export default { login, getRandomNumber };
+export default { login };
---
+++ server/repos/users.js
@@ -0,0 +1,44 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import httpStatus from 'http-status';
+import APIError from '../helpers/APIError';
+import config from '../../config/config';
+import User from '../models/user.model';
+
+const invalidErrorMessage = 'Invalid username or password';
+
+export default {
+  login(loginModel) {
+    return new Promise((resolve, reject) => {
+      if (!loginModel || !loginModel.username || !loginModel.password) {
+        reject(new APIError('Please provide a username and password', httpStatus.BAD_REQUEST, true));
+        return;
+      }
+
+      User
+        .findOne({ username: loginModel.username }, 'username password')
+        .exec()
+        .then((user) => {
+          bcrypt
+            .compare(loginModel.password, user.password)
+            .then((match) => {
+              if (match) {
+                const token = jwt.sign({
+                  username: user.username,
+                  _id: user._id
+                }, config.jwtSecret);
+
+                resolve({
+                  token,
+                  username: user.username
+                });
+              } else {
+                reject(new APIError(invalidErrorMessage, httpStatus.UNAUTHORIZED, true));
+              }
+            })
+            .catch(() => reject(new APIError(invalidErrorMessage, httpStatus.UNAUTHORIZED, true)));
+        })
+        .catch(() => reject(new APIError(invalidErrorMessage, httpStatus.UNAUTHORIZED, true)));
+    });
+  }
+};
